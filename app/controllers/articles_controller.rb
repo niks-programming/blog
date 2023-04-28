13e412a04550c9caa85cfb3a0ac2709d6ff62997
Nik O'Brien <niks.programming@gmail.com>
2023-04-28 16:36:51 -0700

    Create a new view using a params entry

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # create a method for showing an article based on its id
+  def show 
+    @article = Article.find(params[:id])
+  end
 end
