5965c58878cd89216f403994da26ccad6f435526
Nik O'Brien <niks.programming@gmail.com>
2023-04-28 15:02:12 -0700

    Create our first route and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define the root route
+  root "articles#index"
 
+  # Defining a route to say hello
+  # route command "url" to the index action of the articles controller
+  get "/articles", to: "articles#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
