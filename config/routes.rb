63fbb6ba4a3583e16079c31e210ceca50d08c800
Nik O'Brien <niks.programming@gmail.com>
2023-04-30 19:18:15 -0700

    Completing CRUD resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Rails.application.routes.draw do
   # Define the root route
   root "articles#index"
 
-  # Defining a route to show articles
-  # route command "url" to the index action of the articles controller
-  get "/articles", to: "articles#index"
-
-  # Defining a route to show a specific article
-  get "/articles/:id", to: "articles#show"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Resource route
+  resources :articles 
 end
